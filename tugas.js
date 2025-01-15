76ce83a953bcf55a2914146c22ca568100f59970
wahab123456789 <Arilw9020@gmail.com>
2025-01-15 10:51:08 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,17 @@
+
+let nama = "aril"; 
+let alamat = "sayung city"; 
+let usia = "17";  
+let ttl = "demak 4 desember 2007 "; 
+let gender = "laki laki"; 
+let agama = "islam"; 
+let email = "arilw9020@gmail.com";
+let berat = "54";
+let tinggi = "170";
+let hobi = "futsal";
+let cita = "bismillah jepang";
+let pekerjaan = "siswa";
+let sekolah = "SMK N 1 SAYUNG";
+let jurusan = "PPLG";
+
+document.write("nama saya adalah " + nama + " saya tinggal di " + alamat + " pada tahun 2024 ini usia saya adalah " + usia + "tahun, saya lahir di " + ttl + "jenis kelamin saya adalah " + gender + "saya beragama " + agama + "alamat email saya adalah " + email + "saya memiliki berat badan " + berat +"kg"+ "dengan tinggi " + tinggi + "saya sangat senang " + hobi + "karena saya memiliki cita cita " + cita + "saya saat adalah seorang " + pekerjaan + "yang bersekolah di " + sekolah + "pada jurusan " + jurusan);
